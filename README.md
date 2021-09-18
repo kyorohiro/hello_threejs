ad683151c34175f3cdb0fe2001a6da53aff7f1d8
kyorohiro <kyorohiro@gmail.com>
2021-09-19 01:04:47 +0900

    creating seene

---
+++ README.md
@@ -0,0 +1,3 @@
+以下を参考にして、three.js を試してみた
+https://sbcode.net/threejs/webpack-dev-server/
+
---
+++ src/client/webpack.dev.js
@@ -0,0 +1,18 @@
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common.js')
+const path = require('path');
+
+module.exports = merge(common, {
+    mode: 'development',
+    devtool: 'eval-source-map',
+    devServer: {
+        static: {
+            directory: path.join(__dirname, '../../dist/client'),
+        },
+        allowedHosts:[
+            "tetorica.net"
+        ],
+        hot: true,
+    },
+})
+
